662f640f9cbe7a68d6abab1091fa7389894323f0
AistraViter <153955169+AistraViter@users.noreply.github.com>
2024-09-27 00:34:25 +0300

    getAllContacts

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import { env } from './utils/env.js';
+import * as contactServices from "./services/contacts.js";
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -15,17 +16,15 @@ export const setupServer = () => {
   app.use(logger);
   app.use(cors());
   app.use(express.json());
+  app.get('/contacts', async (req, res) => {
+    const data = await contactServices.getAllContacts();
+    res.json({
+      status: 200,
+      message: 'Successfully found contacts!',
+      data,
+    });
+  });
 
-  //   app.use((req, res, next) => {
-  //     console.log(`Time: ${new Date().toLocaleString()}`);
-  //     next();
-  //   });
-
-  // app.get('/', (req, res) => {
-  //   res.json({
-  //     message: 'Hello world!',
-  //   });
-  // });
   app.use('*', (req, res) => {
     res.status(404).json({
       message: 'Route not found',
@@ -43,3 +42,14 @@ export const setupServer = () => {
     console.log(`Server is running on port ${port}`);
   });
 };
+
+//   app.use((req, res, next) => {
+//     console.log(`Time: ${new Date().toLocaleString()}`);
+//     next();
+//   });
+
+// app.get('/', (req, res) => {
+//   res.json({
+//     message: 'Hello world!',
+//   });
+// });
