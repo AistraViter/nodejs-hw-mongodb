511d0efaa1aad6a02c152800599a661dd5684f7b
AistraViter <153955169+AistraViter@users.noreply.github.com>
2024-09-28 00:54:25 +0300

    middlewares replaced from server

--- src/server.js
+++ src/server.js
@@ -2,7 +2,8 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import { env } from './utils/env.js';
-import contactsRouter from "./routers/contacts.js";
+import contactsRouter from './routers/contacts.js';
+import { errorHandler, notFoundHandler } from './middlewares/errorHandler.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -16,37 +17,12 @@ export const setupServer = () => {
   app.use(logger);
   app.use(cors());
   app.use(express.json());
-
   app.use(contactsRouter); // Додаємо роутер до app як middleware
-
-
-  app.use('*', (req, res) => {
-    res.status(404).json({
-      message: 'Route not found',
-    });
-  });
-  app.use((err, req, res, next) => {
-    res.status(500).json({
-      message: 'Something went wrong',
-      error: err.message,
-    });
-  });
+  app.use('*', notFoundHandler);
+  app.use(errorHandler);
 
   const server = app.listen(PORT, () => {
     const port = server.address().port; // Отримуємо призначений порт
     console.log(`Server is running on port ${port}`);
   });
 };
-
-//   app.use((req, res, next) => {
-//     console.log(`Time: ${new Date().toLocaleString()}`);
-//     next();
-//   });
-
-// app.get('/', (req, res) => {
-//   res.json({
-//     message: 'Hello world!',
-//   });
-// });
-
-//
