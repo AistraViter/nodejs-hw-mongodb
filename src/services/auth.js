acf7960ea30c853e1a5c1d17f1fdd52a5f1d3ff0
AistraViter <153955169+AistraViter@users.noreply.github.com>
2024-10-04 02:54:26 +0300

    heshcode added

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,6 +1,14 @@
+import bcrypt from 'bcrypt';
 import { UsersCollection } from '../db/models/user.js';
 
 export const registerUser = async (payload) => {
-  const user = await UsersCollection.create(payload);
+    const encryptedPassword = await bcrypt.hash(payload.password, 10);
+
+  const user = await UsersCollection.create({
+    ...payload,
+    password: encryptedPassword,
+  });
   return user;
 };
+
+ 
