a4cc7f03ec75b1126fd99e7522189a1bc489b88a
AistraViter <153955169+AistraViter@users.noreply.github.com>
2024-10-05 03:03:34 +0300

    refreshed a session!

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { registerUserSchema, loginUserSchema } from '../validation/auth.js';
-import { registerUserController, loginUserController, logoutUserController } from '../controllers/auth.js';
+import { registerUserController, loginUserController, logoutUserController, refreshUserSessionController } from '../controllers/auth.js';
 import { validateBody } from '../middlewares/validateBody.js';
 
 const authRouter = Router();
@@ -21,6 +21,7 @@ authRouter.post(
 authRouter.post('/logout', ctrlWrapper(logoutUserController));
 
 
-export default authRouter;
+authRouter.post('/refresh', ctrlWrapper(refreshUserSessionController));
 
+export default authRouter;
 
