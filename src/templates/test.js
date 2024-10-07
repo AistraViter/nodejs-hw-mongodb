4cdfbe894933a815bed198bb9093d8c65704ac4c
AistraViter <153955169+AistraViter@users.noreply.github.com>
2024-10-08 00:35:05 +0300

    Update test.js

---
+++ src/templates/test.js
@@ -0,0 +1,21 @@
+
+// {
+//     "name": "Lina Ren",
+//     "phoneNumber": "+380000000011",
+//     "email": null,
+//     "isFavourite": true,
+//     "contactType": "home"
+//  }
+
+//  {
+//     "name": "Lina Ren",
+//     "email":  "LinaRen@gmail.com",
+//     "password":  "12345678"
+//  }
+
+//  http://localhost:50632/contacts
+//  http://localhost:50632/auth/login
+
+//  npm run dev
+
+//  Bearer CwmJz/F6CXQdKX7pAOfdRDKVqCQBjUAVSDhDFk0f
